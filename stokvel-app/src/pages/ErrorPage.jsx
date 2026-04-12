53918e0e1d17deb9ae0f9dd6194d725b6e855608
ra2ez <2844961@students.wits.ac.za>
2026-04-12 23:47:35 +0200

    Update ErrorPage.jsx

--- stokvel-app/src/pages/ErrorPage.jsx
+++ stokvel-app/src/pages/ErrorPage.jsx
@@ -1,1 +1,10 @@
+import { Link } from 'react-router-dom'
 
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <Link to="/">Go back home</Link>
+    </div>
+  )
+}
