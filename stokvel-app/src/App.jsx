4617ea49194a871be73f70a81ba8367c93b10a34
ra2ez <2844961@students.wits.ac.za>
2026-04-12 23:49:54 +0200

    added errorpage import and path to App.jsx

--- stokvel-app/src/App.jsx
+++ stokvel-app/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import Home from './pages/Home'
+import ErrorPage from './pages/ErrorPage'
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<div>Dashboard coming soon</div>} />
+        <Route path="*" element={<ErrorPage />} />
         
       </Routes>
     </BrowserRouter>
