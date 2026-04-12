e170bb56e7c2d7def7ddb31125d8d2aa74d19eb4
ahmed786787 <2670859@students.wits.ac.za>
2026-04-12 23:42:20 +0200

    made it look nice and added pictures

--- stokvel-app/src/pages/SignUp.jsx
+++ stokvel-app/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { supabase } from '../supabaseClient'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom' 
 import bgImage from '../assets/background_home.jpg'
 
 const s = {
